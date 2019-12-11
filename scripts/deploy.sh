ea24266e36424320ddb5e2a676192cb3ad6cf56f
Zhang Yang <dimlight1998@gmail.com>
2019-12-11 15:15:39 +0800

    commit before remove .idea

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 sbt assembly
 
 for x in dbvps-{1,2,3}; do
-    scp target/scala-2.12/Toy-Spark-assembly-0.1.jar $x:Toy-Spark
+    scp target/scala-2.12/Toy-Spark-assembly-0.2.jar $x:Toy-Spark
     scp config.json $x:Toy-Spark
 done
