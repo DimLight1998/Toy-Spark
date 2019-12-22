10205372c90b9a3fce8327ccf5cde8473afd39fb
Zhang Yang <dimlight1998@gmail.com>
2019-12-22 16:00:37 +0800

    DAG test

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,10 +6,18 @@ object Main {
   def main(args: Array[String]): Unit = {
     Communication.initialize(args)
 
-    val counter = Dataset.generate(
+    val lhs = Dataset.generate(
       List(4, 4, 4),
-      (nodeID, localPartitionID) => (0 until 100).toList.map(i => i + localPartitionID * 100 + nodeID * 1000))
-    print(counter.count())
+      (nodeID, localPartitionID) => (0 until 10).toList.map(i => i + localPartitionID * 100 + nodeID * 1000))
+
+    print(lhs.count())
+    val rhs = Dataset.generate(
+      List(2, 3, 3),
+      (nodeID, localPartitionID) => (0 until 20).toList.map(i => i + localPartitionID * 100 + nodeID * 1000))
+    val mappedRHS = rhs.map(x => -3 * x).filter(x => x % 2 == 0)
+    val joint     = lhs.unionWith(mappedRHS)
+
+    print(joint.collect(Nil).mkString(", "))
 
     Communication.close()
   }
